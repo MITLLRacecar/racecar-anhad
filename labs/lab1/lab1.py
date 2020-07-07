56c77424a238d0cb244446079eb38419d60842e6
Anhad <sawhney.anhad@gmail.com>
2020-07-07 18:27:14 -0400

    lab 1

--- labs/lab1/lab1.py
+++ labs/lab1/lab1.py
@@ -23,6 +23,8 @@ import racecar_utils as rc_utils
 rc = racecar_core.create_racecar()
 
 # Put any global variables here
+counter = 0
+instruction = []
 
 ########################################################################################
 # Functions
@@ -33,6 +35,13 @@ def start():
     """
     This function is run once every time the start button is pressed
     """
+    # If we use a global variable in our function, we must list it at
+    # the beginning of our function like this
+    global counter
+
+    # The start function is a great place to give initial values to global variables
+    counter = 0
+
     # Begin at a full stop
     rc.drive.stop()
 
@@ -56,19 +65,75 @@ def update():
     After start() is run, this function is run every frame until the back button
     is pressed
     """
+    global counter, instruction
+
     # TODO (warmup): Implement acceleration and steering
-    rc.drive.set_speed_angle(0, 0)
+    if rc.controller.is_down(rc.controller.Button.RB):
+        s = rc.controller.get_trigger(
+            rc.controller.Trigger.RIGHT
+        ) - rc.controller.get_trigger(rc.controller.Trigger.LEFT)
+        a = rc.controller.get_joystick(rc.controller.Joystick.LEFT)[0]
+        rc.drive.set_speed_angle(s, a)
+    else:
+        rc.drive.stop()
 
     if rc.controller.was_pressed(rc.controller.Button.A):
         print("Driving in a circle...")
-        # TODO (main challenge): Drive in a circle
+        # (main challenge): Drive in a circle
+        instruction += [(1, 0.5, 10)]
 
-    # TODO (main challenge): Drive in a square when the B button is pressed
+    # (main challenge): Drive in a square when the B button is pressed
+    if rc.controller.was_pressed(rc.controller.Button.B):
+        print("Driving in a square...")
+        instruction += [
+            (1, 0, 1.5),
+            (1, 1, 1.2),
+            (0, 1, 0.4),
+            (1, 0, 1.5),
+            (1, 1, 1.2),
+            (0, 1, 0.4),
+            (1, 0, 1.5),
+            (1, 1, 1.2),
+            (0, 1, 0.4),
+            (1, 0, 1.5),
+            (1, 1, 1.2),
+            (0, 1, 0.4),
+        ]
 
-    # TODO (main challenge): Drive in a figure eight when the X button is pressed
+    # (main challenge): Drive in a figure eight when the X button is pressed
+    if rc.controller.was_pressed(rc.controller.Button.X):
+        print("Driving in a figure 8...")
+        instruction += [(1, 1, 6), (0, 1, 0.7), (1, -1, 5.8), (0, -1, 0.5)]
 
-    # TODO (main challenge): Drive in a shape of your choice when the Y button
-    # is pressed
+    # (main challenge): Drive in a shape of your choice when the Y button is pressed
+    if rc.controller.was_pressed(rc.controller.Button.Y):
+        print("Driving in a spiral...")
+        instruction += [
+            (1, 1, 1),
+            (1, 0.95, 1),
+            (1, 0.9, 1),
+            (1, 0.85, 1),
+            (1, 0.8, 1),
+            (1, 0.75, 1),
+            (1, 0.7, 1),
+            (1, 0.65, 1),
+            (1, 0.6, 1),
+            (1, 0.55, 1),
+        ]
+
+    if len(instruction) > 0:
+        if counter < instruction[0][2]:
+            rc.drive.set_speed_angle(instruction[0][0], instruction[0][1])
+        else:
+            counter -= instruction[0][2]
+            del instruction[0]
+            if len(instruction) == 0:
+                counter = 0
+                rc.drive.stop()
+
+    # Increases counter by the number of seconds elapsed in the previous frame
+    if len(instruction) > 0:
+        counter += rc.get_delta_time()
 
 
 ########################################################################################
